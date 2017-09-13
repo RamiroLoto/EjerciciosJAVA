d44a2a80175a198336789dcb441a37debbae14ce
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-13 16:53:20 -0300

    Tercer commit
    Se añaden comentarios de ayuda.

--- EjercicioDos.java
+++ EjercicioDos.java
@@ -21,10 +21,16 @@ public class EjercicioDos {
     public static void main(String[] args) {
         // TODO code application logic here
         String Nombre;
+        //Comando String recibe una variable escrita.
         Scanner miescanner = new Scanner (System.in);
+        //Importar la clase "Scanner" permite crear un escaner propio (miescanner)
+        //Y se lo activa utilizando la función "new Scanner (system.in)"  
         System.out.println("Ingrese Nombre");
         Nombre = miescanner.nextLine();
+        //comando ".nextLine" define cuál va a ser la entrada para el Scanner. 
         System.out.println("El nombre es "+ Nombre);
+        //Al final del resultado que se va a mostrar "Concatenamos" la variable 
+        //"Nombre" para que muestre el resultado pedido. 
         
                 
        
