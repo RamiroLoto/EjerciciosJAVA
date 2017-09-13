f7fe1f5d3bad5497b5d98c0b1aa8c473ab210d38
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-13 15:28:13 -0300

    Segundo commit
    Le pusimos comentario.

--- EjercicioUno.java
+++ EjercicioUno.java
@@ -17,17 +17,7 @@ public class EjercicioUno {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int NumeroUno;
-        Scanner misc = new Scanner(System.in);
-        System.out.println("Por favor ingrese un número: ");
-        NumeroUno= misc.nextInt();
-        System.out.println("el numero " + NumeroUno);
-        int NumeroDos;
-        System.out.println("Por favor ingrese otro número: ");
-        Scanner misca = new Scanner(System.in);
-        NumeroDos= misca.nextInt();
-        System.out.println("el número " + NumeroDos);
-        System.out.println("El resultado es: " + NumeroUno "+" NumeroDos);
+       
         // TODO code application logic here
     }
     
