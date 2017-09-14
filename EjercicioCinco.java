14215c7e3b531caa157fddfb5f60e0c234a94496
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-14 20:12:35 -0300

    Primer Commit
    Se comenta el enunciado.

---
+++ EjercicioCinco.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocinco;
+
+/**
+ *
+ * @author Rama
+ */
+public class EjercicioCinco {
+
+    /**
+     * @param args the command line arguments
+     * Se necesita pedir dos números y sumarlos, mostrar el resultado.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
