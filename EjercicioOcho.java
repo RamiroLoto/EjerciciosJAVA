747154712a27f9d98db26f69a67db1a246798c2b
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-14 20:17:32 -0300

    Primer Commit.
    Se comenta el enunciado.

---
+++ EjercicioOcho.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioocho;
+
+/**
+ *
+ * @author Rama
+ */
+public class EjercicioOcho {
+
+    /**
+     * @param args the command line arguments
+     * Se pide el largo y ancho de un terreno u se pide la superficie del mismo.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
