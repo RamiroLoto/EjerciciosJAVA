fd4aa2317519dd6223d01f038600a80aeb3e571c
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-14 20:19:55 -0300

    Primer Commit
    Se comenta el enunciado.

---
+++ EjercicioDiez.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciodiez;
+
+/**
+ *
+ * @author Rama
+ */
+public class EjercicioDiez {
+
+    /**
+     * @param args the command line arguments
+     * Se ingresa la cantidad de ganadores da la lotería nacional 
+     * y el monto total del premio, informar cuánto dinero le corresponde a cada uno.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
