6ae0fbbbf24d4cf23c0ac8d4672109a7bff28c79
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-14 20:21:04 -0300

    Primer Commit
    Se comenta el enunciado.

---
+++ EjercicioOnce.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioonce;
+
+/**
+ *
+ * @author Rama
+ */
+public class EjercicioOnce {
+
+    /**
+     * @param args the command line arguments
+     * Se pide el valor de la hora de trabajo de un empleado y la cantidad 
+     * de horas trabajadas, informar cuanto le corresponde de sueldo, 
+     * restándole el 15% de aportes.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
