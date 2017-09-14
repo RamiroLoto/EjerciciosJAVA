57143f4c2d50236330b6404ef202608618eb7cf3
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-14 20:18:42 -0300

    Primer Commit
    Se comenta el enunciado.

---
+++ EjercicioNueve.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicionueve;
+
+/**
+ *
+ * @author Rama
+ */
+public class EjercicioNueve {
+
+    /**
+     * @param args the command line arguments
+     * Se ingresa la cantidad y precio de un producto y se informa el total a pagar.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
