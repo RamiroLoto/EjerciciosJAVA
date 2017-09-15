ea30e70d757e1d80b47de9a9dab3265fd7bd41ca
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-14 21:08:59 -0300

    Segundo commit.
    Ejercicio terminado.

--- EjercicioCuatro.java
+++ EjercicioCuatro.java
@@ -5,6 +5,8 @@
  */
 package ejerciciocuatro;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rama
@@ -16,8 +18,18 @@ public class EjercicioCuatro {
      * Al ingresar la edad y el nombre mostrar el siguiente mensaje: 
      * “ Usted se llama “ XXXXXX y tiene XX años de edad”.
      */
+
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        String Nombre;
+        int Edad;
+        Scanner misc = new Scanner (System.in);
+        System.out.println("Por favor ingrese su nombre");
+        Nombre = misc.nextLine();
+        System.out.println("Por favor ingrese su edad");
+        Edad = misc.nextInt();
+        System.out.println("Usted se llama "+Nombre+" Y tiene "+Edad+" años de edad");
+        }
+    
     
 }
