672966c7c0f5a00b7aa85dc27aad59308520271a
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-19 09:41:10 -0300

    Segundo commit
    Ejercicio terminado.

--- EjercicioDoce.java
+++ EjercicioDoce.java
@@ -5,6 +5,8 @@
  */
 package ejerciciodoce;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rama
@@ -12,12 +14,21 @@ package ejerciciodoce;
 public class EjercicioDoce {
 
     /**
-     * @param args the command line arguments
-     * Se ingresa el importe de un producto importado y se le debe agregar 
-     * el 35 % de impuestos internos.
+     * @param args the command line arguments Se ingresa el importe de un
+     * producto importado y se le debe agregar el 35 % de impuestos internos.
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner misc = new Scanner(System.in);
+        int Importe;
+        int Impuesto;
+        int Total;
+        System.out.println("Por favor ingrese el importe");
+        Importe = misc.nextInt();
+        Impuesto = (Importe * 35) / 100;
+        Total = Importe + Impuesto;
+        System.out.println("El importa total es " + Total);
+
     }
-    
+
 }
