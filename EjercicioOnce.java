29e6c9b946145a7ab98a12eb20a8b11ce7c21cc1
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-09-19 09:36:56 -0300

    Segundo comit
    Ejercicio terminado.

--- EjercicioOnce.java
+++ EjercicioOnce.java
@@ -5,6 +5,8 @@
  */
 package ejercicioonce;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rama
@@ -12,13 +14,25 @@ package ejercicioonce;
 public class EjercicioOnce {
 
     /**
-     * @param args the command line arguments
-     * Se pide el valor de la hora de trabajo de un empleado y la cantidad 
-     * de horas trabajadas, informar cuanto le corresponde de sueldo, 
-     * restándole el 15% de aportes.
+     * @param args the command line arguments Se pide el valor de la hora de
+     * trabajo de un empleado y la cantidad de horas trabajadas, informar cuanto
+     * le corresponde de sueldo, restándole el 15% de aportes.
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner misc = new Scanner(System.in);
+        int Valor;
+        int Horas;
+        int Sueldo;
+        int Aportes;
+        System.out.println("Por favor ingresar el valor de la hora de trabajo");
+        Valor = misc.nextInt();
+        System.out.println("Por favor ingrese las horas trabajadas");
+        Horas = misc.nextInt();
+        Sueldo = Valor * Horas;
+        Aportes = (Sueldo * 15) / 100;
+        System.out.println("Su sueldo es " + (Sueldo - Aportes) + " pesos");
+
     }
-    
+
 }
