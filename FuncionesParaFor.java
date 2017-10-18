53a4f66a43bd5a0f09d117945ead442476b6d0b2
RamiroLoto <31898588+RamiroLoto@users.noreply.github.com>
2017-10-18 17:51:50 -0300

    Segundo commit
    Mas funciones para for

--- FuncionesParaFor.java
+++ FuncionesParaFor.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
  * @author alumno
  */
 public class FuncionesParaFor {
-
     public static void Saludar() {
         System.out.println("Hola");
     }
@@ -164,8 +163,8 @@ public class FuncionesParaFor {
 
     public static void MostrarCentrosNumericos() {
         int numero;
-        int sumaAnteriores = 0;
-        int sumaPosteriores = 0;
+        int sumaAnteriores;
+        int sumaPosteriores;
         boolean EsCentroNumerico = false;
 
         for (numero = 0; numero < 1000; numero++) {
@@ -198,19 +197,108 @@ public class FuncionesParaFor {
     }
     
     public static void FibonacciSucesion(){
-        int numero=1;
-        int suma=1;
-        int anterior=1;
-        int Fibonacci=0;
-        int auxanterior;
-        for (int contador = 1; contador < 10; contador++) {
-            auxanterior = Fibonacci;
-            Fibonacci = suma+auxanterior;
+        int fibonacci;
+        int anterior=0;
+        int actual=1;
+        for (int contador = 0; contador < 30; contador++) {
+            fibonacci= anterior+actual;
+            anterior=actual;
+            actual=fibonacci;
+            System.out.println("es el: "+fibonacci);
             
-            anterior=Fibonacci;
+        }
+    }
+    
+    public static void RetornarMayor(){
+        Integer NumUno=9, NumDos=6, NumTres=3;
+        
+        if (NumUno>NumDos&&NumUno>NumTres){
+            System.out.println("El primero numero: "+NumUno+" es el mayor");
+        } else if (NumDos>NumUno&&NumDos>NumTres){
+            System.out.println("El segundo numero: "+NumDos+" es el mayor");
+        } else {
+            System.out.println("El tercer numero: "+NumTres+" es el mayor");
+        }
+    }
+    
+    /**
+    * Recorrer los numeros del 1 al 100
+    * Mostrar en lugar del numero la palabra "UTN" para todos 
+    * los multiplos de 3 (tres)
+    * mostrar en lugar del numero la palbra "FRA" para todos 
+    * los multiplos de 5 (tres)
+    * y mostrar la palabra "UTN FRA" para todos los multiplos de 3 y 5. (ej: 15)
+    */
+    
+    public static void SerieUtnFra(){
+        for (int contador = 1; contador < 100; contador++) {
+            if(contador%3==0&&contador%5==0){
+                System.out.println("UTN FRA");}
+            else if(contador%3==0){
+                System.out.println("UTN");
+            } else if(contador%5==0){
+                System.out.println("FRA");
+            } else {
+                System.out.println(contador);
+            }
+        }
+    }
+    
+    /**
+     * 153 = 1^3 + 5^3 + 3^3 = 153 
+     */
+    public static void NumeroArmstrong(){
+        int numeroIngresado=371;
+        
+        int numeroAux=153;
+        int cantidadDigitos=0;
+        
+        //hacemos esto en caso de que el numero sea negativo.
+        if(numeroAux<0){
+            numeroAux*=-1;
+        }
+        
+        while(numeroAux>0){
+            numeroAux/=10;
+            cantidadDigitos++;
+        }
+        
+        int digito;
+        int suma=0;
+        int AuxNumero=numeroIngresado;
+        while(AuxNumero>0){
+        digito=AuxNumero%10;
+        suma+=(int)Math.pow(digito,cantidadDigitos);
+        AuxNumero/=10;}
+        
+        System.out.println(suma);
+        if(suma==numeroIngresado){
+            System.out.println("Es armstrong");
+        }else{
+            System.out.println("No es armstrong");
+        }
+        
+    }
+    
+    public static void HacerPiramide(){
+        int CantidadDeFilas=7;
+        for (int contador = 1; contador < CantidadDeFilas; contador++) {
+            for (int contadorLugares = CantidadDeFilas-1; contadorLugares > contador; contadorLugares--) {
+                System.out.print(" ");
+            }
             
-            suma+=contador;
-            System.out.println("serie "+Fibonacci);
+            for (int contadorDos = 1; contadorDos <=(2*contador-1); contadorDos++) {
+                System.out.print("X");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void MostrarNumeros(){
+        for (int contador = 1; contador < 6; contador++) {
+            for (int contadorDos = 5; contadorDos > contador ; contadorDos--) {
+                System.out.println(contador);
+            }
         }
     }
 }
